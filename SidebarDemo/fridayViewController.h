0e69458e5245013c9b7e0d70aba042c8a0c72aa3
EmersonGGS <Emerson@Emersons-iMac.local>
2014-11-11 23:21:11 -0500

    Adding Pages

---
+++ SidebarDemo/fridayViewController.h
@@ -0,0 +1,13 @@
+//
+//  fridayViewController.h
+//  Humber Fitness
+//
+//  Created by Emerson Stewart on 2014-11-11.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fridayViewController : UIViewController
+
+@end
---
+++ SidebarDemo/tuesdayViewController.h
@@ -0,0 +1,13 @@
+//
+//  tuesdayViewController.h
+//  Humber Fitness
+//
+//  Created by Emerson Stewart on 2014-11-11.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tuesdayViewController : UIViewController
+
+@end
